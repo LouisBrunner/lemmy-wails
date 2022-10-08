5186897a2d745831ef1aafe9385b26f4ae89d6bd
Louis Brunner <louis.brunner.fr@gmail.com>
2022-10-09 00:47:51 +0100

    fix(FE): even better

--- frontend/postcss.config.ts
+++ frontend/postcss.config.ts
@@ -1,7 +1,7 @@
+import tailwindConfig from "./tailwind.config";
 import autoprefixer from "autoprefixer";
-import {resolve} from "path";
 import tailwindcss from "tailwindcss";
 
 export default {
-  plugins: [tailwindcss(resolve(__dirname, "tailwind.config.ts")), autoprefixer, require("autoprefixer")],
+  plugins: [tailwindcss(tailwindConfig), autoprefixer],
 };
